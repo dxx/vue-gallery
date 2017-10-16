7bf6d3693088185ba5d37fff1134b1141a986ca1
code-mcx <1827939333@qq.com>
2017-10-16 23:30:53 +0800

    Init project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <gallery />
+  </div>
+</template>
+
+<script>
+import Gallery from "./components/Gallery.vue"
+
+export default {
+  name: 'app',
+  components: {
+    Gallery
+  }
+}
+</script>
+
+<style>
+html, body{
+  margin: 0;
+  padding: 0;
+}
+html, body, #app{
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
